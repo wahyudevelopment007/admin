811c7d65cc0ad8208257ef470a6cc9298503e3d6
developer <59679642+wahyudevelopment007@users.noreply.github.com>
2021-06-08 21:51:25 +0700

    Update blog-post.js

--- blog-post.js
+++ blog-post.js
@@ -2,7 +2,7 @@ function embedOffice(t) {
    document.write('<div class="drive-responsive style="text-align:center;margin:0 auto;width:100%;"><div style="position:relative;padding-bottom:56.25%;height:0;overflow:hidden;margin:0;"><iframe allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen; style="border:0;position:absolute;top:0;left:0;width:100%;height:100%;" data-src="https://developerapk.000webhostapp.com/files/');
    document.write(t);
    document.write('" allowfullscreen></iframe></div></div>');
-   document.write('<center><a href="https://drive.google.com/uc?export=download&id=');
+   document.write('<center><a href="https://developerapk.000webhostapp.com/');
    document.write(t);
    document.write('"><button class="outline download">Download</button></a></center>');
 }
